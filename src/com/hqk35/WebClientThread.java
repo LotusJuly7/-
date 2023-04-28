75347194ba70690c25939a73799ed289e1c7e706
LotusJuly7 <95471317+LotusJuly7@users.noreply.github.com>
2023-04-28 13:19:24 +0800

    Initial commit

---
+++ src/com/hqk35/WebClientThread.java
@@ -0,0 +1,54 @@
+package com.hqk35;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.os.Handler;
+import android.util.Log;
+
+public class WebClientThread extends Thread {
+	Handler handler;
+	String ipAddress, message;
+	ServerSocket mServerSocket;
+	boolean isLooping = true;
+	public WebClientThread(Handler handler, String ipAddress, String message) {
+		this.handler = handler;
+		this.ipAddress = ipAddress;
+		this.message = message;
+	}
+	@Override
+	public void run() {
+		try {
+            Socket socket = new Socket(ipAddress, MainActivity.port);      //步骤一
+            Log.i("客户端", "得到Socket");
+            socket.setSoTimeout(60000);
+            PrintWriter printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(   //步骤二
+                    socket.getOutputStream(), "UTF-8")), true);
+            Log.i("客户端", "得到PrintWriter");
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+            printWriter.print(message);
+            printWriter.flush();
+            printWriter.close();
+            Log.i("客户端", "flush");
+            /*String receiveMsg;
+            while ((receiveMsg = in.readLine()) != null) {                                      //步骤三
+            	Log.i("客户端", "收到数据：" + receiveMsg);
+            }*/
+            socket.close();
+            Log.i("客户端", "关闭Socket");
+            handler.obtainMessage(1, 0, 0, new Object[] {ipAddress, message}).sendToTarget();
+        } catch (UnknownHostException e) {
+        	handler.obtainMessage(1, 1, 0, "IP地址无效").sendToTarget();
+        } catch (IOException e) {
+            Log.e("客户端", ("连接失败：" + e.getMessage()));   //如果Socket对象获取失败，即连接建立失败，会走到这段逻辑
+        	handler.obtainMessage(1, 1, 0, "连接失败").sendToTarget();
+        }
+	}
+}
---
+++ src/com/hqk35/WebServerThread.java
@@ -0,0 +1,112 @@
+package com.hqk35;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+import android.os.Handler;
+import android.util.Log;
+
+public class WebServerThread extends Thread {
+	Handler handler;
+	ServerSocket mServerSocket;
+	boolean isLooping = true;
+	public WebServerThread(Handler handler) {
+		this.handler = handler;
+	}
+	@Override
+	public void run() {
+		try {
+		    mServerSocket = new ServerSocket(MainActivity.port);
+		    //mServerSocket.setReuseAddress(true);
+		    while (isLooping) {
+		        // 接收客户端套接字。
+		        if (!mServerSocket.isClosed()) {
+		        	try {
+			        	// 阻塞接受客户端。
+			            Socket socket = mServerSocket.accept();
+			            byte[] ipAddress = socket.getInetAddress().getAddress();
+			            Log.i("服务器", "===server accept===");
+				        new ServerThread(socket, handler, ipAddress).start();
+		        	} catch (SocketException e) {
+		        		break;
+		        	}
+		        }
+		    }
+		    if (!mServerSocket.isClosed()) {
+    			mServerSocket.close();
+    		}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void destroy() {
+		isLooping = false;
+		try {
+			mServerSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class ServerThread extends Thread {
+	Socket mSocket;
+	Handler handler;
+	byte[] ipAddress;
+	public ServerThread(Socket socket, Handler handler, byte[] ipAddress) {
+		mSocket = socket;
+		this.handler = handler;
+		this.ipAddress = ipAddress;
+	}
+	@Override
+	public void run() {
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(mSocket.getInputStream(), "utf-8"));
+			Log.i("服务器", "开始线程");
+			String s;
+			StringBuilder result = null;
+			while ((s = reader.readLine()) != null && s.length() >= 0) { // 如果写>0的话，会漏掉空的行
+				Log.i("服务器", "读取行");
+				if (result == null) {
+					result = new StringBuilder();
+				} else {
+					result.append('\n');
+				}
+				result.append(s);
+			}
+			Log.i("服务器", "读取完数据");
+			if (result != null) {
+				String message = result.toString();
+				Log.i("数据", message);
+				handler.obtainMessage(2, new Object[] {ipAddress, message}).sendToTarget();
+			} else {
+				handler.obtainMessage(2, new Object[] {ipAddress, ""}).sendToTarget();
+			}
+            /*// 返回数据
+			OutputStream os = null;
+            try {
+            	os = mSocket.getOutputStream();
+            	os.write("HTTP/1.1 200\r\nContent-Type: text/plain\r\n\r\nHello\r\n".getBytes());
+            	os.flush();
+            	Log.i("服务器", "flush");
+            } finally {
+            	os.close();
+            }*/
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+            try {
+				mSocket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
