e5138fdc363bb4f7b59b6a57069f5925405e54da
LotusJuly7 <95471317+LotusJuly7@users.noreply.github.com>
2023-06-06 22:24:16 +0800

    去掉多余的InputStream

--- src/com/hqk35/WebClientThread.java
+++ src/com/hqk35/WebClientThread.java
@@ -1,9 +1,7 @@
 package com.hqk35;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -32,7 +30,6 @@ public class WebClientThread extends Thread {
             PrintWriter printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(   //步骤二
                     socket.getOutputStream(), "UTF-8")), true);
             Log.i("客户端", "得到PrintWriter");
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
             printWriter.print(message);
             printWriter.flush();
             printWriter.close();
